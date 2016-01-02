eebfbbd4c4a0d8da76e0214cd809b2a4bcaaa15d
sagargestalt <adke.sagar1@gmail.com>
2016-01-02 17:07:48 +0530

    done with contactus functionality

--- app/scripts/services/api-url.js
+++ app/scripts/services/api-url.js
@@ -21,5 +21,6 @@ angular.module('missileManApp')
       'COLLEGE_SEARCH': BASE_URL + 'colleges/search/:id',
       'CUTOFF_DETAILS': BASE_URL + 'cutoff',
       'COLLEGE_CUTOFF': BASE_URL + 'colleges/cutoff/:id',
+       'CONTACTUS': BASE_URL + 'contactus',
     };
   }()));
