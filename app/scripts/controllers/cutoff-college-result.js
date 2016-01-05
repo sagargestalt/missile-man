dfbfcb98a0c00a653802d1c5aeea70aca2192cab
sagargestalt <adke.sagar1@gmail.com>
2016-01-05 14:47:41 +0530

    done with print functionality in college result page

--- app/scripts/controllers/cutoff-college-result.js
+++ app/scripts/controllers/cutoff-college-result.js
@@ -19,4 +19,13 @@ angular.module('missileManApp')
     };
 
     init();
+
+  $scope.printDiv = function(divName) {
+  var printContents = document.getElementById("printable").innerHTML;
+  var popupWin = window.open('', '_blank', 'width=300,height=300');
+  popupWin.document.open()
+  popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</html>');
+  popupWin.document.close();
+};
+
   }]);
