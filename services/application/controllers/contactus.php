4da8c2fb7802062f47412fe048f88d3dcbb30f6d
sagargestalt <adke.sagar1@gmail.com>
2016-01-08 13:35:13 +0530

    resolved email and contact us services

--- services/application/controllers/contactus.php
+++ services/application/controllers/contactus.php
@@ -5,10 +5,10 @@ require APPPATH . '/controllers/CosRestController.php';
 class contactus extends CosRestController
 {
  
- public function index_post()
-  {
+	public function index_post()
+	{
     
-    /*try {*/
+		/*try {*/
          $data = array(
              'first_name' => $this->post('firstname'),
              'last_name' => $this->post('lastname'),
@@ -16,7 +16,7 @@ class contactus extends CosRestController
              'email_address' => $this->post('emailaddress'),
              'message' => $this->post('msg')
             
-         );
+			);
 		$this->sendemail();
 		
         $this->load->database();
@@ -26,59 +26,42 @@ class contactus extends CosRestController
         $this->response(array("data" => array(
            "message" => "User added succefully."
         )));
-       /*} catch(Exception $e) {
+		/*} catch(Exception $e) {
            $this->response(array("data" => array(
            "message" => "Some error occured. Please contact admin."
-       )));
-       }*/
+		)));
+		}*/
   }
-  public function sendemail()
-  {
-
-
-
-
-
-
-
-
-                    //Load the email library
-
-                    $this->load->library('email');
-					$email_address = $this ->input->post('email_address');
-					$message = $this ->input->post('message');
+	public function sendemail()
+	{
+		//Load the email library
+		$this->load->library('email');
+		$email_address = $this ->input->post('email_address');
+		$message = $this ->input->post('message');
 
                     
-                    $this->email->initialize(array("mailtype" => "html"));
-                    $this->email->from($email_address, "user");
+		$this->email->initialize(array("mailtype" => "html"));
+		$this->email->from($email_address, "user");
 
-                   
+		//email to admin
+		$this->email->to('ajitnetwork@gmail.com');
+		$this->email->cc('vishnutekale13@gmail.com');
+		$this->email->subject('userfeedback');
+		$this->email->message($message);
+		$this->email->send();
 
+		//email to user
+					
+		$email_address = $this ->input->post('email_address');
+		$this->email->to($email_address);
+		$this->email->from('info@gitcpl.com', "Admin Team");
+		$this->email->subject("Thank You");
+		$this->email->message("thank you for feedback we will get back to within 2 business days");
+		$this->email->send();
+					 
+					
 
-
-
-                            //$this->email->to('enquiries@lesleynowell.com');
-                            $this->email->to('info@gitcpl.com');
-                            $this->email->subject('userfeedback');
-                            $this->email->message($message);
-
-
-
-                    //If the email is sent
-                    if($this->email->send())
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-
-
-
-        }
-
-
-
+    }
+	 
 }
 ?>
